403a3b035714ed21cb30acd885ca722acffa1c71
Abdallah Mohamed Abdallah Abdelnaby <abdelnaby@sub.uni-goettingen.de>
2026-04-14 13:51:15 +0200

    Configure Cloudflare Workers deployment with OpenNext

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
+
+if (process.env.NODE_ENV === "development") {
+  initOpenNextCloudflareForDev();
+}
+
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
